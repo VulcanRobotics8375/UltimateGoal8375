84e7b603f0572288c26c1f00bd3a0d09e7dd90a8
nicobonik <nicobonik.awesome@gmail.com>
2020-09-12 12:06:08 -0700

    initialized subsystems for ultimategoal

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/subsystems/Intake.java
@@ -0,0 +1,18 @@
+package org.firstinspires.ftc.teamcode.subsystems;
+
+import org.vulcanrobotics.robotcorelib.subsystem.Subsystem;
+
+/**
+ * includes conveyor belt
+ */
+public class Intake extends Subsystem {
+    @Override
+    public void init() {
+
+    }
+
+    @Override
+    public void stop() {
+
+    }
+}
---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/subsystems/WobbleGrabber.java
@@ -0,0 +1,15 @@
+package org.firstinspires.ftc.teamcode.subsystems;
+
+import org.vulcanrobotics.robotcorelib.subsystem.Subsystem;
+
+public class WobbleGrabber extends Subsystem {
+    @Override
+    public void init() {
+
+    }
+
+    @Override
+    public void stop() {
+
+    }
+}
