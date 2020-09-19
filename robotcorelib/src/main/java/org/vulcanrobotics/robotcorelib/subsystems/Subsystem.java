2517f92d1030b36082950b80ed45fedd5ada17a6
nicobonik <nicobonik.awesome@gmail.com>
2020-09-18 22:56:10 -0700

    add telemetry to subsystems

--- robotcorelib/src/main/java/org/vulcanrobotics/robotcorelib/subsystems/Subsystem.java
+++ robotcorelib/src/main/java/org/vulcanrobotics/robotcorelib/subsystems/Subsystem.java
@@ -2,9 +2,14 @@ package org.vulcanrobotics.robotcorelib.subsystems;
 
 import com.qualcomm.robotcore.hardware.HardwareMap;
 
+import org.firstinspires.ftc.robotcore.external.Telemetry;
+import org.vulcanrobotics.robotcorelib.robot.Robot;
+
 public abstract class Subsystem {
 
     public HardwareMap hardwareMap;
+
+    protected Telemetry telemetry = Robot.telemetry;
 
     public Subsystem() {}
 
